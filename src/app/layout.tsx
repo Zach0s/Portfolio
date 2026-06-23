387906ec92d6b86ed73aa85c3b53696c4a8d6e53
zacharie.rodde <zacharie.rodde@epitech.eu>
2026-06-23 12:20:30 +0200

    Ajoute l'intégration de Vercel Speed Insights dans le projet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import GeometricBackground from "@/components/GeometricBackground";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geist = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
           <Footer />
         </Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
